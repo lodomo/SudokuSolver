46e62720c691719983dfb61d50987dd5b25c9259
Lodomo <lodomo.dev@gmail.com>
2025-01-21 16:25:12 -0800

    Changing it up to fix the issues. Progress

--- OLD.SudokuSolver.py
+++ OLD.SudokuSolver.py
@@ -1,7 +1,34 @@
 import sys
 
 
+class Verbose:
+    verbose = False
+
+    def turn_on(self):
+        Verbose.verbose = True
+
+    def print(self, *args):
+        if Verbose.verbose:
+            print(*args)
+
+
 class Cell:
+    """
+    Contains all the logic for a single cell in the Sudoku Board.
+    All rows, columns, and boxes are 0-indexed.
+
+    row:        The row of the cell
+    col:        The column of the cell
+    box:        The box of the cell (0 being the top left, 8 being the bottom right)
+    candidates: A set of all possible values the cell could be.
+    value:      The value of the cell. 0 if the cell is empty.
+    marked:     A boolean value to determine if the cell has been marked for an operation.
+
+    eliminateCandidates:    Removes candidates from the cell that are in the anti_set
+    guaranteeCandidates:    Removes candidates from the cell that are not in the pro_set
+    setVal:                 Sets the value of the cell to val
+    """
+
     def __init__(self, row: int = -1, col: int = -1, value: int = 0):
         self.row = row
         self.col = col
@@ -14,48 +41,37 @@ class Cell:
             self.candidates.clear()
 
     def __str__(self):
-        if self.value:
-            return f"{self.value}"
-        return "~"
+        return f"{self.value}"
 
     def __repr__(self):
-        return self.__str__()
+        return f"{self.value}"
 
-    def set_val(self, val) -> None:
+    def setVal(self, val) -> None:
         """
-        Sets the value of the cell to the given value.
-        The candidates are cleared if the cell has is given a value.
-
-        Raises a ValueError if the cell already has a value.
+        Sets the value of the cell to val
+        Removes all candidates from the cell
+        Returns nothing
         """
-        if self.value:
-            raise ValueError(
-                f"Cell {self.row}, {
-                    self.col} already has a value"
-            )
-
         self.value = val
-        self.candidates.clear()
+        self.candidates = set()
         return
 
-    def remove_candidates(self, set_to_remove) -> int:
+    def eliminateCandidates(self, anti_set: set) -> int:
         """
-        Removes all candidates in to_remove from the cell.
-
-        If the cell has a value, it will not remove any candidates.
-        If the cell has no candidates, it will raise an error.
-
-        Raises a ValueError if the cell no longer has any candidates.
+        Removes candidates from the cell that are in the anti_set
+        Returns 1 if the candidates were changed, 0 if not
         """
         if self.value:
             return 0
 
-        current_candidates = self.candidates.copy()
-        self.candidates -= set_to_remove
-        self.validate()
-        return abs(len(self.candidates) - len(current_candidates))
+        temp = self.candidates.copy()
+        self.candidates -= anti_set
+        if self.candidates == temp:
+            return 0
 
-    def guarantee_candidate(self, pro_set: set) -> int:
+        return 1
+
+    def guaranteeCandidates(self, pro_set: set) -> int:
         """
         Removes candidates from the cell that are not in the pro_set
         Returns 1 if the candidates were changed, 0 if not
@@ -70,17 +86,22 @@ class Cell:
 
         if self.candidates == temp:
             return 0
-        return abs(len(self.candidates) - len(temp))
-
-    def validate(self):
-        """
-        Raises a ValueError if the cell has no candidates, and no value
-        """
-        if not (self.value or len(self.candidates) > 0):
-            raise ValueError(f"Cell {self.row}, {self.col} has no candidates")
+        return 1
 
 
 class Cluster:
+    """
+    Contains all the logic for a row, column or box in the Sudoku Board.
+    Each cluster should have references to all the cells in the cluster.
+    When a cell is modified, it should be updated in ALL 3 clusters it belongs
+    to.
+
+    cells:  A list of all the cells in the cluster
+    values: A set of all the values in the cluster
+
+    addCell:                Adds a cell to the cluster
+    setCell:                Sets the value of the cell at cell_idx to val
+    """
 
     def __init__(self):
         self.cells = []
@@ -95,44 +116,39 @@ class Cluster:
     def __repr__(self):
         return self.__str__()
 
-    def add_cell(self, cell: Cell) -> None:
+    def addCell(self, cell: Cell) -> None:
         """
-        Adds a cell to the cluster
-        Updates the values set with the value of the cell
-
-        Raises a ValueError if the value is already in the cluster
-        Raises a ValueError if the cluster already has 9 cells
+                Adds a cell to the cluster
+                Updates the values set with the value of the cell
+        l
+                *** DOES NOT UPDATE CANDIDATES ***
         """
-        if cell.value in self.values:
-            raise ValueError(f"Value {cell.value} already in cluster")
-
-        if len(self.cells) == 9:
-            raise ValueError("Cluster already has 9 cells")
-
         self.cells.append(cell)
         if cell.value:
             self.values.add(cell.value)
 
-        if len(self.cells) == 9:
-            self.update_candidates()
-        return
-
-    def set_cell(self, val: int, cell: Cell) -> None:
+    def setCell(self, val: int, cell_idx: int):
         """
-        Sets the value of the cell to val
+        Sets the value of the cell at cell_idx to val
         Updates the candidates of all cells in the cluster
-
-        Raises a ValueError if the value is already in the cluster
-        Raises a ValueError if another cell in the cluster has no candidates
+        Returns True if the remaining candidates are valid, False if not
         """
-        if val in self.values:
-            raise ValueError(f"Value {val} already in cluster")
+        self.raiseIfAlreadyInCluster(val)
+        self.cells[cell_idx].setVal(val)
+        self.updateCandidates()
 
-        cell.set_val(val)
-        self.update_candidates()
+        self.raiseIfEmptyCandidates()
         return
 
-    def update_candidates(self) -> None:
+    def initializeCandidates(self) -> None:
+        """
+        Initializes the candidates of all cells in the cluster
+        This assumes that all cells have been added to the cluster
+        """
+        for cell in self.cells:
+            cell.eliminateCandidates(self.values)
+
+    def updateCandidates(self) -> None:
         """
         Makes sure that all cell values have been added to the values set
         Eliminates all candidates that are in the values set
@@ -142,95 +158,24 @@ class Cluster:
                 self.values.add(cell.value)
 
         for cell in self.cells:
-            cell.remove_candidates(self.values)
+            cell.eliminateCandidates(self.values)
 
+    def raiseIfAlreadyInCluster(self, val: int) -> None:
+        """
+        Raises a ValueError if the value is already in the cluster
+        """
+        if val in self.values:
+            raise ValueError("Value already in cluster")
 
-class Board:
-    def __init__(self, data: list, raw_data=False):
-        self.cells = [[Cell(i, j) for j in range(9)] for i in range(9)]
-        self.rows = [Cluster() for _ in range(9)]
-        self.cols = [Cluster() for _ in range(9)]
-        self.boxs = [Cluster() for _ in range(9)]
-        self.solved_cells = 0
+    def raiseIfEmptyCandidates(self):
+        """
+        Validates that all cells in the cluster have valid candidates
+        Returns True if all non-solved cells have candidates, False if not
+        """
+        for cell in self.cells:
+            if not cell.value and not cell.candidates:
+                raise ValueError("Invalid board")
 
-        if raw_data:
-            self.format_data(data)
-
-        self.populateData(data)
-
-    def format_data(self, data):
-        for row in range(9):
-            for col in range(9):
-                if data[row][col] == ".":
-                    data[row][col] = 0
-                else:
-                    data[row][col] = int(data[row][col])
-
-    def populateData(self, board) -> None:
-        for row in range(9):
-            for col in range(9):
-                if board[row][col] != 0:
-                    self.cells[row][col].set_val(int(board[row][col]))
-                    self.solved_cells += 1
-
-                cell = self.cells[row][col]
-                self.rows[row].add_cell(cell)
-                self.cols[col].add_cell(cell)
-                self.boxs[cell.box].add_cell(cell)
-        return
-
-    def printPretty(self):
-        for row in self.cells:
-            for cell in row:
-                print(cell, end=" ")
-            print()
-
-    def printCandidates(self):
-        output = [[" " for _ in range(27)] for _ in range(27)]
-
-        for row in range(9):
-            for col in range(9):
-                start_row, start_col = row * 3, col * 3
-
-                if self.cells[row][col].value:
-                    num = str(self.cells[row][col])
-                    output[start_row + 1][start_col + 1] = num
-                else:
-                    for candidate in self.cells[row][col].candidates:
-                        candidate_row = (candidate - 1) // 3
-                        candidate_col = (candidate - 1) % 3
-                        output[start_row + candidate_row][start_col + candidate_col] = (
-                            str(candidate)
-                        )
-
-        for i, line in enumerate(output):
-            new_line = []
-            for j in range(len(line)):
-                if j % 3 == 0:
-                    new_line.append("|")
-                new_line.append(line[j])
-            new_line.append("|")
-
-            if i % 3 == 0:
-                print("".join(["-" for _ in range(73)]))
-            print(" ".join(new_line))
-        print("".join(["-" for _ in range(73)]))
-
-
-if __name__ == "__main__":
-    board = sys.stdin.read()
-    board = [list(row.strip()) for row in board.split("\n")]
-    if board[-1] == []:
-        board.pop()
-    solver_board = Board(board, raw_data=True)
-    solver_board.printPretty()
-    solver_board.printCandidates()
-    # solver = SudokuSolver(board)
-    # solver.solve()
-    # solver.printCandidates()
-    # print("Remaining combinations:", solver.remainingCombinations())
-
-'''
 
 class SudokuSolver:
     """
@@ -267,7 +212,16 @@ class SudokuSolver:
             string.append(str(row))
         return "\n".join(string)
 
+    def populateData(self, board) -> None:
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] != "." and board[row][col] != 0:
+                    self.board[row][col].setVal(int(board[row][col]))
 
+                self.rows[row].addCell(self.board[row][col])
+                self.cols[col].addCell(self.board[row][col])
+                self.boxs[self.getBox(row, col)].addCell(self.board[row][col])
+        return
 
     def initializeCandidates(self):
         for row in self.rows:
@@ -286,7 +240,37 @@ class SudokuSolver:
     def getBox(self, row, col):
         return (row // 3) * 3 + col // 3
 
+    def printCandidates(self):
+        output = [[" " for _ in range(27)] for _ in range(27)]
 
+        for row in range(9):
+            for col in range(9):
+                start_row, start_col = row * 3, col * 3
+
+                if self.board[row][col].value != 0:  # If the cell has a number
+                    num = str(self.board[row][col])
+                    output[start_row + 1][start_col + 1] = num
+                else:  # If the cell is empty, print candidates
+                    for candidate in self.board[row][col].candidates:
+                        # Map candidate numbers to positions in the 3x3 grid
+                        candidate_row = (candidate - 1) // 3
+                        candidate_col = (candidate - 1) % 3
+                        output[start_row + candidate_row][start_col + candidate_col] = (
+                            str(candidate)
+                        )
+
+        for i, line in enumerate(output):
+            new_line = []
+            for j in range(len(line)):
+                if j % 3 == 0:
+                    new_line.append("|")
+                new_line.append(line[j])
+            new_line.append("|")
+
+            if i % 3 == 0:
+                print("".join(["-" for _ in range(73)]))
+            print(" ".join(new_line))
+        print("".join(["-" for _ in range(73)]))
 
     def solve(self):
         changed = 1
@@ -507,4 +491,18 @@ class SudokuSolver:
                                 {key, key2})
 
         return count
-'''
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "-v":
+        Verbose().turn_on()
+        print("Verbose mode turned on")
+
+    board = sys.stdin.read()
+    board = [list(row.strip()) for row in board.split("\n")]
+    if board[-1] == []:
+        board.pop()
+    solver = SudokuSolver(board)
+    solver.solve()
+    solver.printCandidates()
+    print("Remaining combinations:", solver.remainingCombinations())
